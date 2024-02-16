d969898caccd081b6e24b636bb7eb00ceeff70ec
samimhamza <samim.hamza7@gmail.com>
2024-02-16 23:57:52 +0430

    auth in progress

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validateFields = RegisterSchema.safeParse(values);
@@ -14,9 +15,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
 	const { email, password, name } = validateFields.data;
 	const hashedPassword = await bcrypt.hash(password, 10);
-	const existingUser = await db.user.findUnique({
-		where: { email },
-	});
+	const existingUser = await getUserByEmail(email);
 
 	if (existingUser) {
 		return { error: "Email already in use" };
